8ca249b34edfaa227ce58bb0528f119a3ff76faf
udesh <udesh@kali>
2024-10-16 22:59:36 +0545

    Build conflict one solved

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -5,14 +5,16 @@ import UserModel from "@/model/User";
 import { User } from "next-auth";
 import mongoose from "mongoose";
 
+export async function GET(): Promise<Response> {
+  console.time("GetMessagesExecutionTime"); // Start timing
 
+  await dbConnect();
 
-export async function GET() {
-    await dbConnect();
   const session = await getServerSession(authOptions);
   const user: User = session?.user as User;
 
   if (!session || !session.user) {
+    console.warn("User not authenticated");
     return Response.json(
       {
         success: false,
@@ -24,52 +26,51 @@ export async function GET() {
     );
   }
 
-  
   const userId = new mongoose.Types.ObjectId(user._id);
+  console.log("Get Messages for User ID:", userId);
 
   try {
-    const user= await UserModel.aggregate([
-        { $match :{ _id : userId}},
-        { $unwind :'$messages'},
-        { $sort : {'messages.createdAt' : -1}},
-        { $group : {_id : '$_id', messages: {$push: '$messages'}}},
-        
-    ])
-    if(!user || user.length === 0 ){
-        return Response.json(
-            {
-              success: false,
-              message: `User not found in aggregrate pipeline `,
-            },
-            {
-              status: 401,
-            }
-          );
+    const userMessages = await UserModel.aggregate([
+      { $match: { _id: userId } },
+      { $unwind: '$messages' },
+      { $sort: { 'messages.createdAt': -1 } },
+      { $group: { _id: '$_id', messages: { $push: '$messages' } } },
+    ]);
 
-    }
-    return Response.json(
-        {
-          success: true,
-          messages: user[0].messages,
-        },
-        {
-          status: 201,
-        }
-      );
-
-    
-  } catch (error) {
-    console.error(`Error at catch on getting messages ${error}`)
-    return Response.json(
+    if (!userMessages || userMessages.length === 0) {
+      console.warn("User not found in aggregate pipeline");
+      return Response.json(
         {
           success: false,
-          message:  `Error getting the messages of User `,
+          message: `User not found in aggregate pipeline`,
         },
         {
-          status: 500,
+          status: 404, // Use 404 for not found
         }
       );
+    }
 
-    
+    console.timeEnd("GetMessagesExecutionTime"); // End timing
+    return Response.json(
+      {
+        success: true,
+        messages: userMessages[0].messages,
+      },
+      {
+        status: 200, // Use 200 for success
+      }
+    );
+
+  } catch (error) {
+    console.error(`Error fetching messages: ${error}`);
+    return Response.json(
+      {
+        success: false,
+        message: `Error getting the messages of User`,
+      },
+      {
+        status: 500,
+      }
+    );
   }
 }
