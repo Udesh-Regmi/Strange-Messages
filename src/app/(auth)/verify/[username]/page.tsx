5d08d2ba4cf6708593adfb5d54c3b96299a350ca
udesh <udesh@kali>
2024-10-24 16:59:17 +0545

    Changed UI a little bit and improved photos, sign In , verify and sign Up Ui

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -52,9 +52,9 @@ const VerifyAccount = () => {
     }
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-gray-700 via-gray-900 to-black">
-            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-xl shadow-xl ring-2 ring-indigo-200 ring-opacity-50">
-                <div className="text-center">
+<div className="flex items-center justify-center min-h-screen bg-white">
+<div className="w-full max-w-md p-8 space-y-8 bg-white bg-opacity-40 backdrop-filter backdrop-blur-lg rounded-xl shadow-xl ring-2 ring-indigo-200 ring-opacity-50">
+<div className="text-center">
                     <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-5">
                         Join Anonymous Messages
                     </h1>
