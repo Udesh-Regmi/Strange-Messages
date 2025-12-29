79adeeba76d72228287c73a21d4de3a73262d3ef
Udesh Regmi <udeshregmi00@gmail.com>
2025-12-30 01:16:59 +0545

    Fix Deployment Trial 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-      typescript: {
-    ignoreBuildErrors: true,
+  typescript: {
+    ignoreBuildErrors: false,
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
   images: {
-        domains: ['utfs.io',  'c8r9b7bq24.ufs.sh'  ,'scontent-atl3-1.xx.fbcdn.net','images.unsplash.com'], // Add the required domain here
+    domains: [
+      "utfs.io",
+      "c8r9b7bq24.ufs.sh",
+      "scontent-atl3-1.xx.fbcdn.net",
+      "images.unsplash.com",
+    ], // Add the required domain here
     unoptimized: false,
   },
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
+  output: "standalone",
 };
-
 
 export default nextConfig;
